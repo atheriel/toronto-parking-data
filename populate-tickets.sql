9970c1e1032f23cc2a6a12ca02eca8d08db6c9b6
Aaron Jacobs <atheriel@gmail.com>
2017-06-07 01:07:17 -0400

    Staged tickets are now ordered by date & time.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- populate-tickets.sql
+++ populate-tickets.sql
@@ -1,11 +1,13 @@
 -- Populates the tickets table from the staging table.
 
-INSERT INTO tickets (`date`, infraction, fine, location1, location2, plate)
+CREATE TEMPORARY TABLE tmp_tickets AS
 SELECT
 -- Construct a datetime string from the two components.
-  substr(`date`, 1, 4) || '-' || substr(`date`, 5, 2) || '-' ||
-  substr(`date`, 7, 2) || ' ' || substr(`time`, 1, 2) || ':' ||
-  substr(`time`, 3, 2),
+  CASE WHEN (`date` IS NOT NULL AND `time` IS NOT NULL) THEN
+  (substr(`date`, 1, 4) || '-' || substr(`date`, 5, 2) || '-' ||
+   substr(`date`, 7, 2) || ' ' || substr(`time`, 1, 2) || ':' ||
+   substr(`time`, 3, 2))
+  ELSE NULL END AS dt,
   infraction_code,
   set_fine_amount,
   location2,
@@ -21,5 +23,13 @@ FROM (
     location4,
     province
   FROM tickets_staging
-  LIMIT 10000
+--  LIMIT 1000000
 );
+
+INSERT INTO tickets (`date`, infraction, fine, location1, location2, plate)
+SELECT * FROM tmp_tickets
+ORDER BY datetime(dt);
+
+-- Remove staged data from the staging table.
+
+-- DELETE FROM tickets_staging;
