4f3191a9f8bbeb9da550f0d89a2f4deba4d71f5b
Aaron Jacobs <atheriel@gmail.com>
2017-06-06 23:26:32 -0400

    Cleans up the raw import script, adding compression & reproducibility.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- import-raw-data.sh
+++ import-raw-data.sh
@@ -6,23 +6,39 @@
 # Unzip files.
 
 # for file in raw-data/parking_tickets_*.zip; do
-#     echo "[`date`] Unzipping $file."
-#     unzip $file -d raw-data/
+#     echo "[`date`] Unzipping $file"
+#     unzip $file -d raw-data/ && rm $file
 # done;
 
 # 2010 and 2008 appear to be UTF-16LE encoded, so use iconv to rewrite them.
 
-# echo "[`date`] Converting some files to UTF-8."
+echo "[`date`] Converting some files to UTF-8."
 
-# iconv -f UTF-16LE -t UTF-8 "raw-data/Parking_Tags_data_2008.csv" > "raw-data/Parking_Tags_data_2008.csv.utf8"
-# iconv -f UTF-16LE -t UTF-8 "raw-data/Parking_Tags_data_2010.csv" > "raw-data/Parking_Tags_data_2010.csv.utf8"
+if [ -e "raw-data/Parking_Tags_data_2008.csv"]; then
+    iconv -f UTF-16LE -t UTF-8 "raw-data/Parking_Tags_data_2008.csv" > "raw-data/Parking_Tags_data_2008.csv.utf8"
+    mv "raw-data/Parking_Tags_data_2008.csv.utf8" "raw-data/Parking_Tags_data_2008.csv"
+fi
 
-# mv "raw-data/Parking_Tags_data_2008.csv.utf8" "raw-data/Parking_Tags_data_2008.csv"
-# mv "raw-data/Parking_Tags_data_2010.csv.utf8" "raw-data/Parking_Tags_data_2010.csv"
+if [ -e "raw-data/Parking_Tags_data_2010.csv"]; then
+    iconv -f UTF-16LE -t UTF-8 "raw-data/Parking_Tags_data_2010.csv" > "raw-data/Parking_Tags_data_2010.csv.utf8"
+    mv "raw-data/Parking_Tags_data_2010.csv.utf8" "raw-data/Parking_Tags_data_2010.csv"
+fi
 
-# Read CSV files into the database.
+echo "[`date`] UTF-8 conversion finished."
+
+# Compress CSV files for safekeeping.
 
 for file in raw-data/*.csv; do
+    ( echo "[`date`] Compressing $file"
+      bzip2 -z "$file" ) &
+done;
+wait
+
+echo "[`date`] Compression finished."
+
+# Read compressed CSV files into the database.
+
+for file in raw-data/*.csv.bz2; do
     echo "[`date`] Reading $file into staging table."
-    Rscript copy-csv-to-db.R "$file"
+    Rscript copy-csv-to-db.R "$file" && rm "$file"
 done;
